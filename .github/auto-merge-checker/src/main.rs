d179271f82406362cdb2f433f0774c7005f177bb
statiolake <statiolake@gmail.com>
2024-01-29 01:23:06 +0900

    feat: add auto-merge checker

---
+++ .github/auto-merge-checker/src/main.rs
@@ -0,0 +1,25 @@
+use std::{env, process::Command};
+
+fn get_changed_files() -> Vec<String> {
+    let github_base_ref = env::var("GITHUB_BASE_REF").unwrap();
+    let github_head_ref = env::var("GITHUB_HEAD_REF").unwrap();
+
+    String::from_utf8_lossy(
+        &Command::new("git")
+            .arg("diff")
+            .arg(format!(
+                "origin/{github_base_ref}...origin/{github_head_ref}"
+            ))
+            .output()
+            .unwrap()
+            .stdout,
+    )
+    .lines()
+    .map(|s| s.to_string())
+    .collect()
+}
+
+fn main() {
+    let changed_files = get_changed_files();
+    assert_eq!(changed_files, vec!["README.md"]);
+}
